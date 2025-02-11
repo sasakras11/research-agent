edf642e948a8a4e627f6443a3dd355fbc93e6c3a
Alexander <alexander@Alexanders-MBP.attlocal.net>
2025-02-11 14:30:00 -0800

    added port

--- run.py
+++ run.py
@@ -14,4 +14,4 @@ from src import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, threaded=True)
+    app.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
